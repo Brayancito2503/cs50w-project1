99fb0625f914235daa87811138e53226d9bfe6c2
Brayancito2503 <brayan.potosme03@gmail.com>
2023-09-20 12:50:39 -0600

    api con isbn y correciones estaticas, se agrego el readme

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Project 1
 
 Web Programming with Python and JavaScript
+
+Las carpetas pricipales:
+# Statics
+
+Se encuentran los estilos necesarios para la pagina
+
+# Templates
+
+se encuentran los archivos html que el usuario va a visualizar en su navegador
+
+# Aplication.py
+
+esta toda la logica del backend, tiene funciones espeficicas para cada accion necesaria
+ya sea buscar o guardar informacion desde la base datos, las validaciones sobre formularios
+que ayudan a tener una buena experiencia de usuario
--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session, render_template, redirect, request, url_for, flash
+from flask import Flask, session, render_template, redirect, request, url_for, flash, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -62,8 +62,6 @@ class Resenia:
 def index():
     return redirect('/login')
 
-
-
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     session.clear()
@@ -81,7 +79,6 @@ def login():
             idusuario, nombre,contrasena = result
      
             if result is not None and check_password_hash(result[2], password):
-                # Aquí debes agregar la autenticación exitosa
                 session['user_id'] = idusuario
                 session['nombre'] = nombre
                 print("va al home")
@@ -144,7 +141,7 @@ def register():
         
 @app.route("/home", methods = ["GET", "POST"])
 def home():
-    # Aquí debes implementar la lógica para la página de inicio
+
     if 'user_id' in session:
         if request.method == "POST":
             print("entro al if")
@@ -160,9 +157,9 @@ def home():
                 print("Id libro: " + str(idlibro) + " el isbn: " + isbn + " El titulo es: " + titulo)
                 response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn).json()
                 
-                imagen_libro = None  # Establece la imagen en None por defecto
+                imagen_libro = "https://th.bing.com/th/id/OIP.vDf037OKUo0H03weRxdWuAHaHa?pid=ImgDet&rs=1"  # Establece la imagen por defecto si np tiene
                 
-                libro = {}  # Initialize libro as an empty dictionary
+                libro = {}  
                 if 'items' in response:
                     for item in response['items']:
                         libro = item.get("volumeInfo", {})
@@ -190,125 +187,58 @@ def logout():
     session.clear()
     return redirect("/")
 
-
-@app.route("/search", methods = ["GET", "POST"])
-def search():
+@app.route("/infoLibro/<int:idlibro>")
+def infoLibro(idlibro):
     if 'user_id' in session:
-        if request.method == "POST":
-            print("entro al if")
-            busqueda = request.form.get("busqueda")
-            input = f"%{busqueda}%".lower()
-            print(input)
-            query = text("SELECT * FROM libros WHERE LOWER(isbn) like :isbn OR LOWER(titulo) LIKE :titulo OR LOWER(autor) LIKE :autor OR year LIKE :year")
-            resultado = db.execute(query, {"isbn":input,"titulo":input,"autor":input,"year":input}).fetchall()
-            # for libro in resultado:
-            #     print("IdLibro:", libro.idlibro)
-            #     print("ISBN:", libro.isbn)
-            #     print("Título:", libro.titulo)
-            #     print("Autor:", libro.autor)
-            #     print("Año:", libro.year)
-            #     print("\n")
-          #  print(resultado)
-            # response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:"+resultado[1]).json()
-            # if 'items' in response:
-            #     libro = response['items'][0]['imageLinks']
-            # if 'thumbnail' in primer_libro:
-            #     imagen_libro = primer_libro['thumbnail']
-            libros = [] 
-            print("esta afuera del for")
-            for idlibro, isbn, titulo, autor, anio, year in resultado:
-                print("Id libro: " + str(idlibro) + " el isbn: " + isbn + " El titulo es: " + titulo)
-                response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn).json()
-                
-                imagen_libro = None  # Establece la imagen en None por defecto
-                
-                if 'items' in response:
-                    for item in response['items']:
-                        libro = item.get("volumeInfo", {})
-                        if 'imageLinks' in libro:
-                            imagen_libro = libro['imageLinks'].get('thumbnail', None)
-                            break  # Sal del bucle cuando encuentres una imagen válida
-                
-                descripcion_libro = None
-                
-                if 'description' in libro:
-                    descripcion_libro = libro['description']
-                
-                nuevo_libro = BOOK(isbn, titulo, autor.split(', '), year, idlibro, imagen_libro, descripcion_libro)
-                nuevo_libro.formatear_autor()
-                libros.append(nuevo_libro)
-            return render_template('search.html', libros = libros, nombre= session['nombre'])
-           
-        else:
-            print("si")
-            return render_template("search.html",nombre= session['nombre'])
+        query=text("SELECT * FROM libros WHERE idlibro = :idlibro")
+        resultado = db.execute(query, {"idlibro":idlibro}).fetchone()
+
+        idlibro,isbn, titulo, autor, anio, year = resultado
+
+        print(idlibro,isbn, titulo, autor, anio, year)
+        
+        response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn).json()
+        imagen_libro = "https://th.bing.com/th/id/OIP.vDf037OKUo0H03weRxdWuAHaHa?pid=ImgDet&rs=1"
+        libro = {}    
+        if 'items' in response:
+            for item in response['items']:
+                libro = item.get("volumeInfo", {})
+                if 'imageLinks' in libro:
+                    imagen_libro = libro['imageLinks'].get('thumbnail', None)
+                    break  # Sale del bucle cuando encuentra una imagen válida
+        
+        if 'description' in libro:
+            descripcion_libro = libro['description']
+        cur_book = BOOK(isbn, titulo, autor, year, idlibro,imagen_libro)
+        query = text("SELECT r.IdResenia, r.idUsuario, r.resenian, r.puntaje, r.idlibro, u.nombre FROM resenias r INNER JOIN libros l ON l.idlibro = r.idlibro INNER JOIN usuarios u on r.idusuario = u.idusuario WHERE l.idlibro = :idlibro")
+
+        resenias = db.execute(query, {"idlibro" : idlibro}).fetchall()
+        comentarios=[]
+
+        puntaje = resenias
+        print("asdasd")
+        print(puntaje)
+        print("asdasd")
+        print(resenias)
+        print("asdasd")
+        for IdResenia, idUsuario, resenian, puntaje, idlibro, nombre in resenias:
+            print( IdResenia, idUsuario, resenian, puntaje, idlibro)
+            resenia_nueva = Resenia(IdResenia, idUsuario, resenian, puntaje, idlibro, nombre)
+            comentarios.append(resenia_nueva)
+
+        res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"isbns": cur_book.isbn})
+        print (res)
+        print("si llego")
+        if res.status_code != 200:
+            raise Exception("ERROR: No se pudo acceder a la API.")
+        reseniasLibro = res.json()
+        promedioResenia = float(reseniasLibro['books'][0]['average_rating'])
+        contadorPuntos = float(reseniasLibro['books'][0]['work_ratings_count'])
+        print("antes del render")
+        return render_template("infoLibro.html", book = cur_book, comments = comentarios, promedioResenia = promedioResenia, contadorPuntos = contadorPuntos, nombre= session['nombre'])
     else:
         flash("Debe iniciar sesion")
         return render_template('login.html')
-
-
-@app.route("/results", methods = ["POST"])
-def results():
-    if request.method == "POST":
-        book_search = request.form.get("book_search")
-        input = f"%{book_search}%".lower()
-        look_up = db.execute("SELECT * FROM libros WHERE isbn LIKE :isbn OR LOWER(title) LIKE :title OR LOWER(author) LIKE :author OR year LIKE :year",{"isbn":input,"title":input,"author":input,"year":input}).fetchall()
-        books= []
-        for isbn, titulo, autor, year, idlirbo in look_up:
-            new_book = Book(isbn, title, author.split(', '), year, book_id, imagen)
-            new_book.trim_author()
-            books.append(new_book)
-
-        return render_template("results.html",results = books)
-
-@app.route("/infoLibro/<int:idlibro>")
-def infoLibro(idlibro):
-    query=text("SELECT * FROM libros WHERE idlibro = :idlibro")
-    resultado = db.execute(query, {"idlibro":idlibro}).fetchone()
-
-    idlibro,isbn, titulo, autor, anio, year = resultado
-
-    print(idlibro,isbn, titulo, autor, anio, year)
-    
-    response = requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn).json()
-    
-    libro = {}    
-    if 'items' in response:
-        for item in response['items']:
-            libro = item.get("volumeInfo", {})
-            if 'imageLinks' in libro:
-                imagen_libro = libro['imageLinks'].get('thumbnail', None)
-                break  # Sale del bucle cuando encuentra una imagen válida
-    
-    if 'description' in libro:
-        descripcion_libro = libro['description']
-    cur_book = BOOK(isbn, titulo, autor, year, idlibro,imagen_libro)
-    query = text("SELECT r.IdResenia, r.idUsuario, r.resenian, r.puntaje, r.idlibro, u.nombre FROM resenias r INNER JOIN libros l ON l.idlibro = r.idlibro INNER JOIN usuarios u on r.idusuario = u.idusuario WHERE l.idlibro = :idlibro")
-
-    resenias = db.execute(query, {"idlibro" : idlibro}).fetchall()
-    comentarios=[]
-
-    puntaje = resenias
-    print("asdasd")
-    print(puntaje)
-    print("asdasd")
-    print(resenias)
-    print("asdasd")
-    for IdResenia, idUsuario, resenian, puntaje, idlibro, nombre in resenias:
-        print( IdResenia, idUsuario, resenian, puntaje, idlibro)
-        resenia_nueva = Resenia(IdResenia, idUsuario, resenian, puntaje, idlibro, nombre)
-        comentarios.append(resenia_nueva)
-
-    res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"isbns": cur_book.isbn})
-    print (res)
-    print("si llego")
-    if res.status_code != 200:
-        raise Exception("ERROR: API request unsuccessful.")
-    good_read = res.json()
-    promedioResenia = float(good_read['books'][0]['average_rating'])
-    contadorPuntos = float(good_read['books'][0]['work_ratings_count'])
-    print("antes del render")
-    return render_template("infoLibro.html", book = cur_book, comments = comentarios, promedioResenia = promedioResenia, contadorPuntos = contadorPuntos, nombre= session['nombre'])
 
 @app.route("/resenia", methods=["POST"])
 def post_comment():
@@ -316,15 +246,49 @@ def post_comment():
         idUsuario = session["user_id"]
         idlibro = request.form.get("book_id")
         resenian=request.form.get("book_comment")
-        puntaje = float(request.form.get("rating"))
+        puntaje = float(request.form.get("rating")) if request.form.get("rating") is not None else 0
 
         query = text("SELECT * FROM resenias WHERE idUsuario = :idUsuario AND idlibro = :idlibro")
         check = db.execute(query,{"idUsuario":idUsuario, "idlibro":idlibro}).fetchone()
         if check==None:
-            query = text("INSERT INTO resenias (idUsuario, resenian, puntaje, idlibro) VALUES (:idUsuario, :resenian, :puntaje, :idlibro)")
-            db.execute(query,{"idUsuario":idUsuario, "resenian":resenian, "puntaje":puntaje, "idlibro":idlibro})
-            db.commit()
+            if puntaje != 0:
+                query = text("INSERT INTO resenias (idUsuario, resenian, puntaje, idlibro) VALUES (:idUsuario, :resenian, :puntaje, :idlibro)")
+                db.execute(query,{"idUsuario":idUsuario, "resenian":resenian, "puntaje":puntaje, "idlibro":idlibro})
+                db.commit()
+            else:
+                flash("Debe dar una puntuacion")
         else:
             flash("Solo puede hacer una reseña por libro")
-
+        
         return redirect(url_for("infoLibro", idlibro = idlibro), "303")
+
+
+@app.route("/api/<string:Pisbn>")
+def externalapi(Pisbn):  
+    P_isbn=f"%{Pisbn}%".lower()
+    query = text("SELECT * FROM libros WHERE LOWER(isbn) LIKE :isbn")
+    res = db.execute(query,{"isbn":P_isbn}).fetchone()
+    if res is None:
+        return jsonify({
+        "Error":404,
+        "Mensaje de error": f"No se encontro el libro con ISBN: {Pisbn}"
+        }), 404
+
+   
+    
+    idlibro, isbn, titulo, autor, anio, year = res
+    libro =  BOOK(isbn, titulo, autor, year, idlibro, " ")
+    
+    res2 = requests.get("https://www.goodreads.com/book/review_counts.json", params={"isbns": libro.isbn})
+    reseniasLibro = res2.json()
+    promedioResenia = float(reseniasLibro['books'][0]['average_rating'])
+    contadorPuntos = float(reseniasLibro['books'][0]['work_ratings_count'])
+    result = {
+    "Title": libro.titulo,
+    "author": libro.autor,
+    "year": libro.year,
+    "isbn": libro.isbn,
+    "review_count": contadorPuntos,
+    "average_score": promedioResenia
+    }
+    return jsonify(result)       
